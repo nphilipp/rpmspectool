979c88d3db4c68c4f9eb9b86ecc7a4e005287fcd
Nils Philippsen <nils@redhat.com>
2015-11-20 14:36:56 +0100

    version 1.99.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info.major < 3:
 
 setup(
     name="rpmspectool",
-    version="1.99.2",
+    version="1.99.3",
     author="Nils Philippsen",
     author_email="nils@redhat.com",
     url="https://github.com/nphilipp/rpmspectool",
